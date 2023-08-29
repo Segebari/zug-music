c1a1be2ac907f22394103e6288df0cdc6a34ae69
Segebari <segebarifred@gmail.com>
2023-08-29 18:57:57 +0100

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,44 @@
+const songsContainer = document.querySelector('.new-release');
+let isDragging = false;
+let startPosition = 0;
+let scrollLeft = 0;
+
+songsContainer.addEventListener('mousedown', (e) => {
+    isDragging = true;
+    startPosition = e.clientX;
+    scrollLeft = songsContainer.scrollLeft;
+
+    songsContainer.style.cursor = 'grabbing';
+});
+
+songsContainer.addEventListener('mouseup', () => {
+    isDragging = false;
+    songsContainer.style.cursor = 'grab';
+});
+
+songsContainer.addEventListener('mouseleave', () => {
+    isDragging = false;
+    songsContainer.style.cursor = 'grab';
+});
+
+songsContainer.addEventListener('mousemove', (e) => {
+    if (!isDragging) return;
+    const deltaX = e.clientX - startPosition;
+    songsContainer.scrollLeft = scrollLeft - deltaX;
+});
+
+songsContainer.addEventListener('touchstart', (e) => {
+    isDragging = true;
+    startPosition = e.touches[0].clientX;
+    scrollLeft = songsContainer.scrollLeft;
+});
+
+songsContainer.addEventListener('touchend', () => {
+    isDragging = false;
+});
+
+songsContainer.addEventListener('touchmove', (e) => {
+    if (!isDragging) return;
+    const deltaX = e.touches[0].clientX - startPosition;
+    songsContainer.scrollLeft = scrollLeft - deltaX;
+});
