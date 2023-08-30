12ccc6b7904ce58b6d25849dc8d5ba0a7904647e
Segebari <segebarifred@gmail.com>
2023-08-30 16:34:27 +0100

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -42,3 +42,12 @@ songsContainer.addEventListener('touchmove', (e) => {
     const deltaX = e.touches[0].clientX - startPosition;
     songsContainer.scrollLeft = scrollLeft - deltaX;
 });
+
+
+const menuBtn = document.getElementById('menu-btn');
+const navMenu = document.getElementById('nav');
+
+
+menuBtn.addEventListener('click', () => {
+    navMenu.classList.toggle('navhidden');
+  });
